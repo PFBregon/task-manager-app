60322a1dd5e0fe8e73c0e9d44808a62c1710a1b0
PFBregon <bregon@gmail.com>
2025-08-18 12:40:58 +0200

    backend restructuring finally working ok

--- backend/src/routes/tasks.routes.js
+++ backend/src/routes/tasks.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Task = require('../models/Task');
+const Task = require('../models/tasks');
 
 let tasks = [
     { id: 1, title: "Aprender Node.js", completed: false },
